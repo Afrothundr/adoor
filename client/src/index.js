357602625aeb0ab2b62eba8053b28d818d9772e6
Branford <branfordharris@Branfords-MacBook-Pro.local>
2018-02-20 10:48:33 -0600

    added api routes for react and update file arch

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from 'react-dom';
+import App from './App';
 
-class App extends Component {
-    render() {
-      return(
-        <h1>Hello World</h1>
-      );
-    }
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ client/src/utils/API.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export default {
+    getUsers: function() {
+        return axios.get("/api/users");
+    },
+    getUser: function(id) {
+        return axios.get("/api/users/" + id);
+    },
+    createUser: function(user) {
+        return axios.post("/api/users", user);
+    },
+    getListings: function() {
+        return axios.get("/api/listing/");
+    },
+    getListing: function(id) {
+        return axios.get("/api/listing/" + id);
+    },
+    createListing: function(sellerId, listing) {
+        return axios.post("/api/listing", listing);
+    },
+    getSellers: function() {
+        return axios.get("/api/seller");
+    },
+    getSeller: function(id) {
+        return axios.get("/api/seller/" + id);
+    },
+    createMatch: function(userId, listingId) {
+        return axios.post("/api/match", listingId);
+    },
+    addMatch: function(userId, listingId) {
+        return axios.put("/api/match", listingId);
+    },
+    createPref: function(userId, preferences) {
+        return axios.post("/api/match", preferences);
+    },
+    updatePref: function(userId, preferences) {
+        return axios.put("/api/preferences", preferences);
+    },
+    createCommunity: function(listingId, community) {
+        return axios.post("/api/community", listingId);
+    }
+};

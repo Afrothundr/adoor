bc513141ba12c12eb8c68b13443ff53bf9f22b07
Nick Herrera <nickherrera34@gmail.com>
2018-02-25 10:39:41 -0600

    added standard side bar and placeholder in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import UserPrefQuiz from './components/UserPrefQuiz/UserPrefQuiz';
+import StandardSideBar from './components/standardSideBar/standardSideBar';
 
 const App = props => {
   return (
     <MuiThemeProvider>
       <UserPrefQuiz />
+      <StandardSideBar />
     </MuiThemeProvider>
+    
   );
 }
 

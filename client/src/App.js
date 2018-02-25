e41b9d129ab3ed2d0880fe52c7b986ea7fcab62c
Nick Herrera <nickherrera34@gmail.com>
2018-02-25 10:58:11 -0600

    added matching sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import UserPrefQuiz from './components/UserPrefQuiz/UserPrefQuiz';
 import StandardSideBar from './components/standardSideBar/standardSideBar';
+import MatchingSideBar from './components/matchingSideBar/matchingSideBar';
 
 const App = props => {
   return (
     <MuiThemeProvider>
       <UserPrefQuiz />
       <StandardSideBar />
+      <MatchingSideBar />
     </MuiThemeProvider>
     
   );

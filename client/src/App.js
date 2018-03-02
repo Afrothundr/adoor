b31fcdf60afa4c1d06a09f91c1f3e919184a40bb
Branford <branford.harris@outlook.com>
2018-03-01 20:29:08 -0600

    matching page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import UserPrefQuiz from './components/UserPrefQuiz/UserPrefQuiz';
+// import UserPrefQuiz from './components/UserPrefQuiz/UserPrefQuiz';
 //import StandardSideBar from './components/standardSideBar/standardSideBar';
-import MatchingSideBar from './components/matchingSideBar/matchingSideBar';
-import BuySignUp from './components/BuyAcctLogin/BuyerAcctLogin';
-import SellerAcctLogin from './components/SellerAcctLogin/SellerAcctLogin';
-import BuyerAcctLogin from './components/BuyAcctLogin/BuyerAcctLogin';
+// import MatchingSideBar from './components/matchingSideBar/matchingSideBar';
+// import BuySignUp from './components/BuyAcctLogin/BuyerAcctLogin';
+// import SellerAcctLogin from './components/SellerAcctLogin/SellerAcctLogin';
+// import BuyerAcctLogin from './components/BuyAcctLogin/BuyerAcctLogin';
+import MatchingContainer from './components/Matching/MatchingContainer';
 
 
 const App = props => {
   return (
-    <MuiThemeProvider>
-      <BuyerAcctLogin />
-    </MuiThemeProvider>
+      <MatchingContainer />
     
   );
 }
--- client/src/components/Matching/MatchingContainer.js
+++ client/src/components/Matching/MatchingContainer.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import Slider from 'react-slick';
 import ListingCard from './ListingCard';
 import './MatchingContainer.css'
-// import API from '../../utils/API';
-
+import API from '../../utils/API';
+import { checkForCommunityMatch, checkForLocationMatch } from '../../utils/MatchingAlgorithm';
 
 
 class MatchingContainer extends Component {
@@ -12,27 +12,67 @@ class MatchingContainer extends Component {
         //intial state
         this.state = {
             //replace userId with stored cookie value
-            userId: '5a8f3ab48896fd45f054117d'
+            userId: '5a8f3ab48896fd45f054117d',
+            listings: [1, 2, 3, 4]
         }
+        this.next = this.next.bind(this)
+    }
+    componentWillMount() {
+        API.getUser(this.state.userId).then(user => {
+            this.setState({user: user.data});
+        })
     }
     //handle decision
     handleDecision = decision => {
         if (decision === 'Yes') {
             console.log('I like this');
+            this.checkForMatch(this.state.user, "5a887bd865e859b4acd2f60d");
+            this.next();
         } else {
             console.log('I hate this');
+            this.next();
         }
+    }
+
+    checkForMatch = (user, listingId) => {
+        API.getListing(listingId).then(listing => {
+            if(!user.matches) {
+                let score = (this.compareCompatibility(user, listing.data));
+                if (score > 2) alert("It's a match!");
+            } else {
+                let score = (this.compareCompatibility(user, listing.data));
+                if (score > 2) alert("It's a match!");
+            }
+        })
+    }
+
+    compareCompatibility = (user, listing) => {
+        let communityScore = checkForCommunityMatch(user, listing);
+        let locationScore = checkForLocationMatch(user, listing);
+        let score = communityScore + locationScore;
+        return score;
+
+    }
+
+    handleSlideChange = (currentSlide, nextSlide) => {
+        console.log(currentSlide);
+       this.state.listings.splice(currentSlide);
+    }
+
+    next() {
+        this.slider.slickNext();
     }
 
     render() {
         var settings = {
-            arrows: false
+            arrows: false,
+            beforeChange: this.handleSlideChange
         };
         return (
-            <Slider {...settings} className="matching-container" style={{width: "500px"}}>
+            <Slider ref={c => this.slider = c } {...settings} className="matching-container" style={{width: "500px"}}>
                 <div><ListingCard key={0} title="3 bed 2 bath" onDecision={this.handleDecision} /></div>
-                <div><ListingCard key={1}/></div>
-                <div><ListingCard key={2}/></div>
+                <div><ListingCard key={1} onDecision={this.handleDecision}/></div>
+                <div><ListingCard key={2} onDecision={this.handleDecision}/></div>
             </Slider>
         );
     }

2f9d79012ba549f8185cf56af541cc80362db19e
Branford <branford.harris@outlook.com>
2018-03-02 20:14:55 -0600

    added one way data pasing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import UserPrefQuiz from './components/UserPrefQuiz/UserPrefQuiz';
-import StandardSideBar from './components/standardSideBar/standardSidebar';
-import MatchingSideBar from './components/matchingSideBar/matchingSideBar';
-import BuySignUp from './components/BuyAcctLogin/BuyerAcctLogin';
-import SellerAcctLogin from './components/SellerAcctLogin/SellerAcctLogin';
-import MyMatches from './components/myMatches/myMatches';
-import MyListings  from './components/MyListings/MyListingsTable';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import UserPrefQuiz from './components/UserPrefQuiz/UserPrefQuiz';
+// import StandardSideBar from './components/standardSideBar/standardSidebar';
+// import MatchingSideBar from './components/matchingSideBar/matchingSideBar';
+// import BuySignUp from './components/BuyAcctLogin/BuyerAcctLogin';
+// import SellerAcctLogin from './components/SellerAcctLogin/SellerAcctLogin';
+// import MyMatches from './components/myMatches/myMatches';
+// import MyListings  from './components/MyListings/MyListingsTable';
+import MatchingContainer from './components/Matching/MatchingContainer';
+
 
 const App = props => {
   return (
-      {/* <UserPrefQuiz />
+      /* <UserPrefQuiz />
       <StandardSideBar />
       <MatchingSideBar />
       <BuySignUp />
-      <SellerAcctLogin /> */}
-      <MyMatches />
-      {/* <BuyerAcctLogin /> */}
-      <MyListings />
+      <SellerAcctLogin /> */
+      // <MyMatches />
+      /* <BuyerAcctLogin /> */
+      // <MyListings />
+      <MatchingContainer />
     
   );
 }

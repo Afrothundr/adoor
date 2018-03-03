20870d03992aa879782f530a0eca282a6dcccad9
Nick Herrera <nickherrera34@gmail.com>
2018-03-02 18:42:19 -0600

    added components into pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import UserPrefQuiz from './components/UserPrefQuiz/UserPrefQuiz';
-import StandardSideBar from './components/standardSideBar/standardSidebar';
-import MatchingSideBar from './components/matchingSideBar/matchingSideBar';
-import BuySignUp from './components/BuyAcctLogin/BuyerAcctLogin';
-import SellerAcctLogin from './components/SellerAcctLogin/SellerAcctLogin';
-import MyMatches from './components/myMatches/myMatches';
-import MyListings  from './components/MyListings/MyListingsTable';
+import SellerDashboard from './pages/sellerDashboard';
+import BuyerProfileCreate from './pages/buyerCreateProfile';
+import BuyerLogin from './pages/buyerLogin';
+import SellerAcctLogin from './pages/sellerLogin'
 
 const App = props => {
   return (
     <div>
-      {/* <UserPrefQuiz />
-      <StandardSideBar />
-      <MatchingSideBar />
-      <BuySignUp />
       <SellerAcctLogin />
-      <MyMatches />
-      <BuyerAcctLogin /> */}
-      <MyListings />
     </div>
   );
 }

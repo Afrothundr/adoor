ca9007bad56703392b81974ec3efcb6b2945b077
Branford <branford.harris@outlook.com>
2018-03-04 23:17:32 -0600

    Added fade route transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 import SellerDashboard from './pages/sellerDashboard';
 import BuyerProfileCreate from './pages/buyerCreateProfile';
 import BuyerLogin from './pages/buyerLogin';
@@ -7,18 +9,27 @@ import SellerAcctLogin from './pages/sellerLogin';
 import Matching from './pages/matching';
 import Home from './pages/index';
 
+
 const App = props => {
   return (
-    <Router history={Router}>
-    <div>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/matching" component={Matching}></Route>
-      <Route exact path="/seller/dashboard" component={SellerDashboard}></Route>
-      <Route exact path="/profile/login" component={BuyerLogin}></Route>
-      <Route exact path="/profile/create" component={BuyerProfileCreate}></Route>
-      <Route exact path="/seller/login" component={SellerAcctLogin}></Route>
-    </div>
-    </Router>
+    <Route render={({ location }) => (
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          timeout={300}
+          classNames='fade'
+        >
+          <Switch location={ location }>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/matching" component={Matching}></Route>
+            <Route exact path="/seller/dashboard" component={SellerDashboard}></Route>
+            <Route exact path="/profile/login" component={BuyerLogin}></Route>
+            <Route exact path="/profile/create" component={BuyerProfileCreate}></Route>
+            <Route exact path="/seller/login" component={SellerAcctLogin}></Route>
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    )} />
   );
 }
 

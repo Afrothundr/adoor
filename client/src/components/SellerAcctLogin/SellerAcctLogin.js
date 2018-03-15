6848c76b7e887a103cd56d4a6d8cc3e68ae7fd3c
Luissette Figueroa <luissette.figueroa@gmail.com>
2018-03-14 19:42:41 -0500

    google oaut logout

--- client/src/components/SellerAcctLogin/SellerAcctLogin.js
+++ client/src/components/SellerAcctLogin/SellerAcctLogin.js
@@ -1,25 +1,112 @@
 import React from 'react';
-import FacebookLoginButton from 'react-social-login-buttons/lib/buttons/FacebookLoginButton';
-import GoogleLoginButton from 'react-social-login-buttons/lib/buttons/GoogleLoginButton';
-import "./SellerAcctLogin.css";
+import {Route, Redirect} from 'react-router';
+import { Router, Switch } from 'react-router-dom';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 import axios from 'axios';
+import "./SellerAcctLogin.css";
 
-const responseGoogle = () => {
-    axios.get('/auth/google');
-    //console.log("Response from google: " + response);
+const styles = {
+    display: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignText: 'center'
+    }
 };
-  
-  const SellSignUp = () => (
-    <div className='component-wrapper'>
-        <div>
-            <h1 className='signUp'>Account Login</h1>
-            <hr/>
+
+class BuySignUp extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: null,
+            firstTimeUser: null
+        };
+    }
+
+    responseGoogle(response){
+        axios.post('/auth/google/user', response)
+        .then(res => {
+            console.log(res.data);
+            document.cookie = `userId=${res.data._id}`;
+            console.log(document.cookie);
+            if(res.data.preferences){
+                console.log("Not a new user google in promise");
+                this.setState({
+                    firstTimeUser: false,
+                    isLoggedIn: true
+                })
+            }else{
+                console.log("New user in google axios promise");
+                this.setState({
+                    firstTimeUser: true,
+                    isLoggedIn: true,                    
+                })
+            }
+            console.log("first time user? " + this.state.firstTimeUser);
+        })
+        .catch(function (error) {
+            console.log(error);
+          });
+    };
+    
+    responseFacebook(response){
+        axios.post('/auth/facebook/user', response)
+        .then(res => {
+            console.log(res.data);
+            document.cookie = `userId=${res.data._id}`;
+            console.log(document.cookie);
+            if(res.data.preferences){
+                console.log("Not a new user in axios facebook promise");
+                this.setState({
+                    firstTimeUser: false,
+                    isLoggedIn: true
+                })
+            }else{
+                console.log("New user in axios facebook promise");
+                this.setState({
+                    firstTimeUser: true,
+                    isLoggedIn: true,                    
+                })
+            }
+            console.log("first time user? " + this.state.firstTimeUser);
+        })
+        .catch(function (error) {
+            console.log(error);
+          });
+    };
+    
+    logout(){
+        axios.post('/auth/google/api/logout');
+    }
+
+
+    render(){
+        if(this.state.isLoggedIn === true){
+            return <Redirect to={"/seller/dashboard"} />;
+        }
+        return(        
+        <div className='component-wrapper'>
+            <div>
+                <h1 className='signUp'>Sign Up</h1>
+            </div>
+            <div className='buttons'>
+                {/* <FacebookLogin
+                    appId="927418800748086"
+                    autoLoad={false}
+                    fields="name,email,picture"
+                    callback={this.responseFacebook.bind(this)}
+                /> */}
+                <GoogleLogin
+                    clientId='786832441182-3vhl05ve7u3ee3fr35j477lnv2gqv2h0.apps.googleusercontent.com'
+                    buttonText="Login with Google"
+                    onSuccess={this.responseGoogle.bind(this)}
+                    onFailure={this.responseGoogle.bind(this)}
+                />
+                
+            </div>
         </div>
-        <div className='buttons'>
-            <FacebookLoginButton onClick={() => alert('Hello')} />
-            <GoogleLoginButton onClick={responseGoogle} />
-        </div>
-    </div>
-  );
-  
-export default SellSignUp;
+        )}
+};
+
+export default BuySignUp;
--- client/src/pages/sellerDashboard.js
+++ client/src/pages/sellerDashboard.js
@@ -2,13 +2,17 @@ import React from "react";
 import MyListingsTable from '../components/MyListings/MyListingsTable';
 import AddListingForm from '../components/MyListings/AddListingForm';
 import Header from '../components/Header/Header';
+import SellerBuyerLogout from "../components/SellerBuyerLogout/SellerBuyerLogout";
 import './gridStyles.css';
 
 const SellerDashboard = props => {
   return (
     <div className="seller-dashboard-wrapper page">
-      <div className="header">
-      <Header />
+      <div>
+        <Header />
+        <div>
+          <SellerBuyerLogout />
+        </div>
       </div>
       <div className="wrapper-main">
         <div className="content-main">

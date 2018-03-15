ad9c4c600bc5636bc06d7e15378948911d5070b6
Avery Johnson <averymjohnson@outlook.com>
2018-03-15 12:55:14 -0500

    finished styling page with drawer, needs colors

--- client/src/components/MatchingButton/MatchingButton.js
+++ client/src/components/MatchingButton/MatchingButton.js
@@ -1,15 +1,51 @@
-import React, { Component } from "react";
+import React, { Component} from "react";
 import './button.css';
+import Drawer from 'material-ui/Drawer';
+import MyMatches from "..//myMatches/myMatches.js";
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 class MatchingButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+    handleToggle = () => this.setState({open: !this.state.open});
+
+    handleClose = () => this.setState({open: false});
+     
+    onRequestChange = open => this.setState({open});
+
     render() {
         return (
-            <div className="button-component">
-                <img className="matching-button" src={require('../../pages/imgs/myMatchesButton.png')} id='logo' alt="logo" />
-            </div>
-        )
+          <div className='container'>  
+          <div>
+            <RaisedButton
+              label="My Matches"
+              onClick={this.handleToggle}
+            />
+        </div>
+        <div className='drawershit'>
+            <Drawer
+              docked={false}
+              width={'50%'}
+              height={'100%'}
+              open={this.state.open}
+              onRequestChange={(open) => this.setState({open})}
+            >
+            <h2>Your Matches</h2>
+            <MyMatches style={{width:'100%'}} />
 
+            </Drawer>
+            </div>
+          </div>
+        );
+      }
     }
-}
 
 export default MatchingButton
+
+
+
+
--- client/src/pages/matching.js
+++ client/src/pages/matching.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, {Component}from "react";
 import MatchingContainer from '../components/Matching/MatchingContainer';
 import MyMatches from '../components/myMatches/myMatches';
 import MatchingButton from '../components/MatchingButton/MatchingButton';
 import './gridStyles.css';
 
-const Matching = props => {
+
+class Matching extends Component {
+  constructor() {
+    super();
+    //intial state
+    this.state = {
+      userHasMatches: false,
+    }
+}
+
+handleuserHasMatches = user => {
+  if (user.matches){
+    this.setState({
+      userHasMatches: true
+    });
+  }
+}
+
+render() {
   return (
-    <div className="matching-dashboard-wrapper page">
-      <div className="my-matches-button">
-      <MatchingButton />
+    <div className="matching-dashboard-wrapper-page">
+    <div className="my-matches-button">
+       {this.state.userHasMatches ? (
+        <MatchingButton />
+        ) : null}
       </div>
-      <div>
+    <div className="logo">
+    <img src= {require('./imgs/logo.png')} className="logo"/>
+    </div>
         <div className="matching-main">
-          <MatchingContainer />
+          <MatchingContainer userMatches={this.handleuserHasMatches} /> 
         </div>
-        <div>
-          <MyMatches />
-        </div>
+      <div>
       </div>
     </div>
   );
+}
+
 }
 
 export default Matching;

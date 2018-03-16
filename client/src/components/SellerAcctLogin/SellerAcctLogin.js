363e1cead8447a1875f451daea6a27fb5ce30cab
Nick Herrera <nickherrera34@gmail.com>
2018-03-15 20:42:55 -0500

    updated syling yet again

--- client/src/components/SellerAcctLogin/SellerAcctLogin.js
+++ client/src/components/SellerAcctLogin/SellerAcctLogin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Redirect} from 'react-router';
+import { Route, Redirect } from 'react-router';
 import { Router, Switch } from 'react-router-dom';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
@@ -14,7 +14,7 @@ const styles = {
     }
 };
 
-class BuySignUp extends React.Component{
+class BuySignUp extends React.Component {
 
     constructor(props) {
         super(props);
@@ -24,90 +24,94 @@ class BuySignUp extends React.Component{
         };
     }
 
-    responseGoogle(response){
+    responseGoogle(response) {
         axios.post('/auth/google/user', response)
-        .then(res => {
-            console.log(res.data);
-            document.cookie = `userId=${res.data._id}`;
-            console.log(document.cookie);
-            if(res.data.preferences){
-                console.log("Not a new user google in promise");
-                this.setState({
-                    firstTimeUser: false,
-                    isLoggedIn: true
-                })
-            }else{
-                console.log("New user in google axios promise");
-                this.setState({
-                    firstTimeUser: true,
-                    isLoggedIn: true,                    
-                })
-            }
-            console.log("first time user? " + this.state.firstTimeUser);
-        })
-        .catch(function (error) {
-            console.log(error);
-          });
+            .then(res => {
+                console.log(res.data);
+                document.cookie = `userId=${res.data._id}`;
+                console.log(document.cookie);
+                if (res.data.preferences) {
+                    console.log("Not a new user google in promise");
+                    this.setState({
+                        firstTimeUser: false,
+                        isLoggedIn: true
+                    })
+                } else {
+                    console.log("New user in google axios promise");
+                    this.setState({
+                        firstTimeUser: true,
+                        isLoggedIn: true,
+                    })
+                }
+                console.log("first time user? " + this.state.firstTimeUser);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
-    
-    responseFacebook(response){
+
+    responseFacebook(response) {
         axios.post('/auth/facebook/user', response)
-        .then(res => {
-            console.log(res.data);
-            document.cookie = `userId=${res.data._id}`;
-            console.log(document.cookie);
-            if(res.data.preferences){
-                console.log("Not a new user in axios facebook promise");
-                this.setState({
-                    firstTimeUser: false,
-                    isLoggedIn: true
-                })
-            }else{
-                console.log("New user in axios facebook promise");
-                this.setState({
-                    firstTimeUser: true,
-                    isLoggedIn: true,                    
-                })
-            }
-            console.log("first time user? " + this.state.firstTimeUser);
-        })
-        .catch(function (error) {
-            console.log(error);
-          });
+            .then(res => {
+                console.log(res.data);
+                document.cookie = `userId=${res.data._id}`;
+                console.log(document.cookie);
+                if (res.data.preferences) {
+                    console.log("Not a new user in axios facebook promise");
+                    this.setState({
+                        firstTimeUser: false,
+                        isLoggedIn: true
+                    })
+                } else {
+                    console.log("New user in axios facebook promise");
+                    this.setState({
+                        firstTimeUser: true,
+                        isLoggedIn: true,
+                    })
+                }
+                console.log("first time user? " + this.state.firstTimeUser);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
-    
-    logout(){
+
+    logout() {
         axios.post('/auth/google/api/logout');
     }
 
 
-    render(){
-        if(this.state.isLoggedIn === true){
+    render() {
+        if (this.state.isLoggedIn === true) {
             return <Redirect to={"/seller/dashboard"} />;
         }
-        return(        
-        <div className='component-wrapper'>
-            <div>
-                <h1 className='signUp'>Sign Up</h1>
-            </div>
-            <div className='buttons'>
-                {/* <FacebookLogin
+        return (
+            <div className='component-wrapper'>
+                <div className="signin-logo">
+                    <img src={require('./logo.png')} className="logo" />
+                </div>
+                <div>
+                    <h1 className='signUp'>Sign Up</h1>
+                </div>
+                <div>
+                    {/* <FacebookLogin
                     appId="927418800748086"
                     autoLoad={false}
                     fields="name,email,picture"
                     callback={this.responseFacebook.bind(this)}
                 /> */}
-                <GoogleLogin
-                    className="google-login-button"
-                    clientId='786832441182-3vhl05ve7u3ee3fr35j477lnv2gqv2h0.apps.googleusercontent.com'
-                    buttonText="Login with Google"
-                    onSuccess={this.responseGoogle.bind(this)}
-                    onFailure={this.responseGoogle.bind(this)}
-                />
-                
+                    <GoogleLogin
+                        className="google-login-button"
+                        clientId='786832441182-3vhl05ve7u3ee3fr35j477lnv2gqv2h0.apps.googleusercontent.com'
+                        buttonText="Login with Google"
+                        onSuccess={this.responseGoogle.bind(this)}
+                        onFailure={this.responseGoogle.bind(this)}
+                    />
+
+                </div>
             </div>
-        </div>
-        )}
+        )
+    }
 };
 
 export default BuySignUp;
